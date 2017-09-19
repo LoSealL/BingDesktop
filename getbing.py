c147d13208fe9d9a6907c536fdf48f6ef9fbd797
Wenyi Tang <twytwy12345@live.com>
2017-09-19 11:33:45 +0800

    add CI test

--- getbing.py
+++ getbing.py
@@ -1,78 +1,76 @@
 ﻿#coding=utf-8
+import re, time, os, sys
+import win32gui, win32api, win32con
 import urllib.request as urlget
-import re
-import time
 from PIL import Image
-import os,sys
-import win32gui,win32api,win32con
 
 class bingPaper():
-    def __init__(self,homepage):
-        self.homepage = homepage
-        print("Useage: %s" %sys.argv[0])
-        print("Proxy Useage: %s [IP Address] [Port]" %sys.argv[0])
-        if len(sys.argv) == 3:
-            self.server = sys.argv[1]
-            self.port = sys.argv[2]
-            print("Proxy server IP is: %s" %sys.argv[1])
-            proxyHandle = urlget.ProxyHandler({"http":'http://'+self.server+':'+self.port})
-            passwd = urlget.HTTPPasswordMgrWithDefaultRealm()
-            proxyPassHandle = urlget.ProxyBasicAuthHandler(passwd)
-            # proxyPassHandle.add_password(None,self.server,"loseall","169253moevpn")
-            opener = urlget.build_opener(proxyHandle,proxyPassHandle,urlget.HTTPHandler)
-            urlget.install_opener(opener)
-        page = urlget.urlopen(self.homepage)
-        self.html = page.read()
-    def getDate(self):
-        date = time.strftime('%Y-%m-%d',time.localtime())
-        return date
-    def getImg(self):
-        spec_reg = False
-        reg1 = "(?<=g_img\=\{url\:..)+http\://s\.cn\.bing\.net/\w+/\w+/\w+/\w+-\w+1920x1080.jpg"
-        reg2 = "(?<=g_img\=\{url\:..)+/\w+/\w+/\w+/\w+-\w+1920x1080.jpg"
-        imgre1 = re.compile(reg1)
-        imgre2 = re.compile(reg2)
-        imglist = re.findall(imgre1,str(self.html))
-        if not imglist:
-            imglist = re.findall(imgre2,str(self.html))
-            spec_reg = True
-            if not imglist:
-                fd = open('err.html','w')
-                fd.writelines(str(self.html))
-                fd.close()
-                return None
-        for imgurl in imglist:
-            if not spec_reg:
-                imgurl = imgurl.replace('\\','')
-            else:
-                imgurl = "http://cn.bing.com" + imgurl            
-            print(imgurl)
-            urlget.urlretrieve(imgurl,'[%s]%s' % (self.getDate(),imgurl[-20:]))
-        return '['+self.getDate()+']'+imgurl[-20:]
+  '''
+  homepage: must be "http://cn.bing.com"
+  I havn't tested on other bing sites.
+  '''
+  def __init__(self, homepage):
+    print("Useage: %s" %sys.argv[0])
+    print("Proxy Useage: %s [IP Address] [Port]" %sys.argv[0])
+    self.homepage = homepage
+    if len(sys.argv) == 3:
+      # using proxy server
+      server = sys.argv[1]
+      port = sys.argv[2]
+      # using http proxy, you can modify this to meet your requirement
+      print("Proxy server IP is: {}:{}".format(server, port))
+      proxyProtocol = "http"
+      proxyHandle = urlget.ProxyHandler({proxyProtocol: "{}://{}:{}".format(proxyProtocol, server, port)})
+      passwd = urlget.HTTPPasswordMgrWithDefaultRealm()
+      proxyPassHandle = urlget.ProxyBasicAuthHandler(passwd)
+      # if your proxy has password, add your password here
+      # proxyPassHandle.add_password(None, server, "user", "12345678")
+      opener = urlget.build_opener(proxyHandle,proxyPassHandle,urlget.HTTPHandler)
+      urlget.install_opener(opener)
+    page = urlget.urlopen(self.homepage)
+    self.html = page.read()
+    
+  '''
+  get the background image url from the page
+  '''
+  def getImg(self):
+    # this reg was deprecated since 2017.1
+    reg1 = "(?<=g_img\=\{url\:..)+http\://s\.cn\.bing\.net/\w+/\w+/\w+/\w+-\w+1920x1080.jpg"
+    reg2 = "(?<=g_img\=\{url\:..)+/\w+/\w+/\w+/\w+-\w+1920x1080.jpg"
+    imgre2 = re.compile(reg2)
+    imglist = re.findall(imgre2,str(self.html))
+    if not imglist:
+      fd = open('err.html','w')
+      fd.writelines(str(self.html))
+      fd.close()
+      return None
+    date = time.strftime('%Y-%m-%d', time.localtime())
+    for imgurl in imglist:
+      imgurl = "http://cn.bing.com" + imgurl
+      print(imgurl)
+      urlget.urlretrieve(imgurl, "[{}]{}".format(date, imgurl[-20:]))
+    return "[{}]{}".format(date, imgurl[-20:])
+  
+  """
+  Given a path to an image, convert it to bmp and set it as wallpaper
+  """
+  def setWallPaper(self):
+    file = self.getImg()
+    if not file:
+      return
+    print(file)
+    Image.open(file).save('mywallpaper.bmp', "BMP")
+    self.setWallpaperFromBMP(os.getcwd() + '\\mywallpaper.bmp')
+  
+  '''
+  Using pywin32 to set windows desktop wallpaper
+  imagepath should be absolute path
+  '''
+  def setWallpaperFromBMP(self, imagepath):
+    k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+    win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "2")  #2拉伸适应桌面,0桌面居中
+    win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, imagepath, 3)
 
-class setPaper():
-    def __init__(self,imagepath):
-        self.imagepath = imagepath
-        self.StoreFolder = os.getcwd()
-    def setWallpaperFromBMP(self,imagepath):
-        k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
-        win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "2") #2拉伸适应桌面,0桌面居中
-        win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
-        win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,imagepath, 1+2)
-    def setWallPaper(self):
-        """
-        Given a path to an image, convert it to bmp and set it as wallpaper
-        """
-        bmpImage = Image.open(self.imagepath)
-        newPath = self.StoreFolder + '\\mywallpaper.bmp'
-        bmpImage.save(newPath, "BMP")
-        self.setWallpaperFromBMP(newPath)
-        
-myhtml = bingPaper("http://cn.bing.com")
-fName = myhtml.getImg()
-if not fName:
-    exit(0)
-path = os.getcwd()
-print(fName)
-wallpaper = setPaper(path+'\\'+fName)
-wallpaper.setWallPaper()
+# app starts
+bingPaper("http://cn.bing.com").setWallPaper()
--- setDesktop.bat
+++ setDesktop.bat
@@ -1,16 +1,19 @@
 @echo off
-d:
-cd d:\Works\BingDesktop
-python getbing.py
-if EXIST *.jpg (
-    move /Y *.jpg wallpaper
+REM make a wallpaper to store past images
+IF NOT EXIST wallpaper (
+    mkdir wallpaper
 )
-if EXIST mywallpaper.bmp (
-    if EXIST err.html (
-        del err.html /Q /F
-    )
-) else (
+REM call python script (python3 required)
+python getbing.py
+REM we use converted bmp file as wallpaper, and store downloaded jpg(s).
+IF EXIST *.jpg (
+    MOVE /Y *.jpg wallpaper
+)
+REM fail to download image from bing.com
+IF NOT EXIST mywallpaper.bmp (
     @echo Can't get bing.com
     pause
+    IF EXIST err.html (
+        DEL err.html /Q /F
+    )
 )
-REM pause
