6a9b6760b63029120e23cf4d8d4d59177010d3de
Wenyi Tang <wenyi.tang@intel.com>
2019-09-15 13:17:11 +0800

    Add user header

--- install.bat
+++ install.bat
@@ -1,29 +1,6 @@
 @echo off
 @echo Check pyBindDesktop requirements
-pip show image > .check.cache
-pip show pywin32 >> .check.cache
-
-set foundImage=FALSE
-set foundPywin32=FALSE
-for /f "tokens=2 delims=: " %%i in (.check.cache) do (
-  if %%i==image set foundImage=TRUE
-  if %%i==pywin32 set foundPywin32=TRUE
-)
-
-if not %foundPywin32%==TRUE (
-  echo Please download pywin32 that fits your python version
-  python -V
-  explorer "https://github.com/mhammond/pywin32/releases"
-) else (
-  echo found pywin32
-)
-if not %foundImage%==TRUE (
-  pip install Image
-) else (
-  echo found image
-  del /Q /F .check.cache
-)
-
+pip install pywin32 Image pillow requests lxml
 REM a directory to store downloaded pictures
 if not exist wallpaper mkdir wallpaper
 
